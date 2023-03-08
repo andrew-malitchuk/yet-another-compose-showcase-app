5c2da52809d80634b4bac41479df02ebbc51462d
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-03-08 21:53:02 +0200

    YACSA-1 Migrate to gradle.kts

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "dev.yacsa.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "yacsa.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        
+        register("androidApplication") {
+            id = "yacsa.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+//        register("androidLibraryCompose") {
+//            id = "yacsa.android.library.compose"
+//            implementationClass = "AndroidLibraryComposeConventionPlugin"
+//        }
+//        register("androidLibrary") {
+//            id = "yacsa.android.library"
+//            implementationClass = "AndroidLibraryConventionPlugin"
+//        }
+        register("androidFeature") {
+            id = "yacsa.android.feature"
+            implementationClass = "AndroidFeatureConventionPlugin"
+        }
+//        register("androidHilt") {
+//            id = "yacsa.android.hilt"
+//            implementationClass = "AndroidHiltConventionPlugin"
+//        }
+//        register("androidFlavors") {
+//            id = "yacsa.android.application.flavors"
+//            implementationClass = "AndroidApplicationFlavorsConventionPlugin"
+//        }
+    }
+}

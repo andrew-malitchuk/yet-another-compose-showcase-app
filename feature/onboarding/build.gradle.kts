fe088c5e5ac39a31008ab9b83cd1d0c25fc69fd4
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-03-12 17:38:20 +0200

    YACSA-2 Add :feature:onboarding

---
+++ feature/onboarding/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("yacsa.android.feature")
+    id("yacsa.android.library.compose")
+}
+
+android {
+    namespace = "dev.yacsa.onboarding"
+    buildFeatures{
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.navigation.compose)
+    implementation("androidx.compose.material:material:1.3.1")
+
+//    implementation("androidx.compose.ui:ui-tooling-preview:1.3.3")
+//    implementation("androidx.compose.ui:ui-tooling:1.3.3")
+
+
+    implementation(project(":feature:navigation"))
+    implementation(project(":core:ui"))
+
+}

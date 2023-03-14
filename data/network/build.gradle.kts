37701e262edcd2296a8aa737edd976ae58877ead
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-03-14 23:45:04 +0200

    YACSA-3 Add missed dependencies

--- data/network/build.gradle.kts
+++ data/network/build.gradle.kts
@@ -9,5 +9,7 @@ android {
 
 dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
-    implementation("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
+
+
 }

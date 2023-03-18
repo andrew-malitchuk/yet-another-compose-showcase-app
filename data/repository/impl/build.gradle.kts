7b4e59e41a5840c5fccd180c0078d68371195b49
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-03-18 22:37:34 +0200

    YACSA-4 Fix issues

---
+++ data/repository/impl/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("yacsa.android.library")
+    id("yacsa.android.hilt")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "dev.yacsa.repository.impl"
+}
+
+dependencies {
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    implementation("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation(libs.retrofit.core)
+
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.1")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.1")
+
+    debugImplementation("com.github.chuckerteam.chucker:library:3.5.2")
+    releaseImplementation("com.github.chuckerteam.chucker:library-no-op:3.5.2")
+
+
+    implementation(project(":data:network"))
+}

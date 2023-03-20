bff5f212ed52068db33eedbb18655afe83dc3035
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-03-20 23:20:35 +0200

    YACSA-10 Add StartUpConfigureDataStoreModel

---
+++ data/datastore/src/main/kotlin/dev/yacsa/datastore/source/StartupConfigureDataStoreSource.kt
@@ -0,0 +1,7 @@
+package dev.yacsa.datastore.source
+
+import dev.yacsa.datastore.model.StartUpConfigureDataStoreModel
+import dev.yacsa.datastore.source.base.BaseDataStoreSource
+
+abstract class StartupConfigureDataStoreSource :
+    BaseDataStoreSource<StartUpConfigureDataStoreModel>()

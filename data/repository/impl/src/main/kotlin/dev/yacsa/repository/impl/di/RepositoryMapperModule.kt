ee1df6a3669eac824b79e716b0c3e588c68ab1a6
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-03-21 23:33:43 +0200

    YACSA-10 Add StartUpConfigureRepoDataStoreMapper

---
+++ data/repository/impl/src/main/kotlin/dev/yacsa/repository/impl/di/RepositoryMapperModule.kt
@@ -0,0 +1,20 @@
+package dev.yacsa.repository.impl.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.yacsa.repository.impl.mapper.StartUpConfigureRepoDataStoreMapper
+
+
+@Suppress("UnnecessaryAbstractClass")
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryMapperModule {
+
+    @Provides
+    fun provideGreetingMapper(): StartUpConfigureRepoDataStoreMapper {
+        return StartUpConfigureRepoDataStoreMapper()
+    }
+
+}
---
+++ data/repository/impl/src/main/kotlin/dev/yacsa/repository/impl/mapper/StartUpConfigureRepoDataStoreMapper.kt
@@ -0,0 +1,24 @@
+package dev.yacsa.repository.impl.mapper
+
+import dev.yacsa.datastore.model.StartUpConfigureDataStoreModel
+import dev.yacsa.repository.impl.mapper.base.RepoDataStoreMapper
+import dev.yacsa.repository.model.StartUpConfigureRepoModel
+import javax.inject.Inject
+
+class StartUpConfigureRepoDataStoreMapper @Inject constructor() :
+    RepoDataStoreMapper<StartUpConfigureRepoModel, StartUpConfigureDataStoreModel>() {
+
+    override fun toRepo(value: StartUpConfigureDataStoreModel): StartUpConfigureRepoModel {
+        return StartUpConfigureRepoModel(
+            value.hasBeenOnboardingShown
+        )
+    }
+
+    override fun toDataStore(value: StartUpConfigureRepoModel): StartUpConfigureDataStoreModel {
+        return StartUpConfigureDataStoreModel(
+            value.hasBeenOnboardingShown
+        )
+    }
+
+
+}

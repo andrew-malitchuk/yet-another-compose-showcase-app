f29821ba6ae90c0f47468e96f8227976b7e16592
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-04-10 21:46:49 +0300

    YACSA-9 Add service:featureflag

---
+++ service/featureflag/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("yacsa.android.library")
+    id("yacsa.android.hilt")
+    id("kotlinx-serialization")
+}
+
+android {
+    namespace = "dev.yacsa.featureflag"
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+}

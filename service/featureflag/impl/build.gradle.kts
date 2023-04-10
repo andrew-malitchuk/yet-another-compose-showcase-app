32ad16b3656cebf50c321e0c7cf240e69d015887
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-04-10 21:48:03 +0300

    YACSA-9 Clean-up code

---
+++ service/featureflag/impl/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("yacsa.android.library")
+    id("yacsa.android.hilt")
+    id("kotlinx-serialization")
+}
+
+android {
+    namespace = "dev.yacsa.featureflag.impl"
+}
+
+dependencies {
+    implementation("androidx.datastore:datastore:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+
+    implementation(project(":service:featureflag"))
+    implementation(project(":feature:books"))
+}

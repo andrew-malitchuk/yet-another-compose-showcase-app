810bf677b014c55d8f1af3f0297223d348818757
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-05-01 21:39:35 +0300

    YACSA-11 Add mappers

---
+++ domain/impl/src/main/kotlin/dev/yacsa/domain/impl/mapper/SearchHistoryDomainRepoMapper.kt
@@ -0,0 +1,20 @@
+package dev.yacsa.domain.impl.mapper
+
+import dev.yacsa.domain.impl.mapper.base.DomainRepoMapper
+import dev.yacsa.domain.model.SearchHistoryDomainModel
+import dev.yacsa.repository.model.SearchHistoryRepoModel
+import javax.inject.Inject
+
+
+class SearchHistoryDomainRepoMapper@Inject constructor() :
+    DomainRepoMapper<SearchHistoryDomainModel, SearchHistoryRepoModel>() {
+
+    override fun toDomain(value: SearchHistoryRepoModel): SearchHistoryDomainModel {
+        return SearchHistoryDomainModel(value.query?:"")
+    }
+
+    override fun toRepo(value: SearchHistoryDomainModel): SearchHistoryRepoModel {
+        return SearchHistoryRepoModel(value.query?:"")
+    }
+
+}

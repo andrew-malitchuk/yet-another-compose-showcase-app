6022441effe43b206cc2b6fb3b8311b90ac2a732
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-05-03 22:59:42 +0300

    YACSA-37 Add mapstruct

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -10,4 +10,11 @@ android {
 
 dependencies {
     implementation(project(":domain"))
+// https://mvnrepository.com/artifact/org.mapstruct/mapstruct-processor
+    kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    // https://mvnrepository.com/artifact/org.mapstruct/mapstruct
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+// https://mvnrepository.com/artifact/org.mapstruct/mapstruct-jdk8
+    implementation("org.mapstruct:mapstruct-jdk8:1.5.5.Final")
+
 }

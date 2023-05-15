a7e7d7b5887cb52f5778748d6f1059231e22b77b
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-05-15 22:38:08 +0300

    YACSA-26 Add local provider

---
+++ service/analytics/provider/impl/local/src/main/kotlin/dev/yacsa/provider/local/LocalAnalyticProvider.kt
@@ -0,0 +1,29 @@
+package dev.yacsa.provider.local
+
+import dev.yacsa.analytics.event.ContentViewAnalyticModel
+import dev.yacsa.analytics.event.CustomAnalyticModel
+import dev.yacsa.analytics.event.UserPropertyAnalyticModel
+import dev.yacsa.analytics.event.base.EventAnalyticModel
+import dev.yacsa.provider.AnalyticProvider
+import logcat.logcat
+import javax.inject.Inject
+
+class LocalAnalyticProvider @Inject constructor() : AnalyticProvider {
+
+    override fun log(event: EventAnalyticModel) {
+        when (event) {
+            is ContentViewAnalyticModel -> {
+                logcat { "ContentViewAnalyticModel: ${event.viewName}" }
+            }
+
+            is CustomAnalyticModel -> {
+                logcat { "CustomAnalyticModel: $event" }
+            }
+        }
+    }
+
+    override fun setProperty(property: UserPropertyAnalyticModel) {
+        logcat { "CustomAnalyticModel: {${property.key} : ${property.value}}" }
+    }
+
+}

9aa0b2ecda0f649b7fb1ff278eb928033565afbf
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-05-23 20:27:45 +0300

    YACSA-46 Add collapsing toolbar

--- feature/books/src/main/kotlin/dev/yacsa/books/screen/detalization/composable/toolbar/CollapsingToolbar.kt
+++ feature/books/src/main/kotlin/dev/yacsa/books/screen/detalization/composable/toolbar/CollapsingToolbar.kt
@@ -1,30 +1,26 @@
 package dev.yacsa.books.screen.detalization.composable.toolbar
 
-import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,6 +28,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -48,42 +45,13 @@ import dev.yacsa.books.R
 import dev.yacsa.model.model.BookUiModel
 import dev.yacsa.ui.theme.YacsaTheme
 
-//https://github.com/arpit999/ComposeDemo/tree/collapsing_toolbar
-@Composable
-fun Bar( book: BookUiModel?,) {
-    val lazyScrollState = rememberLazyListState()
-    Scaffold(
-        modifier = Modifier
-            .fillMaxSize(),
-        topBar = {
-            if (book != null) {
-                CollapsingToolbar(lazyScrollState,book)
-            }
-        },
-    ) { paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)) {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = Color.White)
-                    .animateContentSize(),
-                state = lazyScrollState
-            ) {
-                items(100) { index ->
-                    Text(modifier = Modifier.padding(36.dp), text = "Item: $index")
-                    Divider(color = Color.Black, thickness = 1.dp)
-                }
-
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalMotionApi::class)
+@OptIn(ExperimentalMotionApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun CollapsingToolbar(
     lazyScrollState: LazyListState,
-    book: BookUiModel
+    book: BookUiModel,
+    onBackClick:()->Unit,
+    onDownloadClick:()->Unit
 ) {
     val context = LocalContext.current
     val motionScene = remember {
@@ -91,20 +59,24 @@ fun CollapsingToolbar(
     }
 
     val progress by animateFloatAsState(
-        targetValue = if (lazyScrollState.firstVisibleItemIndex in 0..1) 0f else 1f,
-        tween(5000)
+//        targetValue = if (lazyScrollState.firstVisibleItemIndex in 0..1) 0f else 1f,
+//        targetValue = if (!lazyScrollState.canScrollBackward) 0f else 1f,
+        targetValue = if (!lazyScrollState.canScrollBackward && !lazyScrollState.isScrollInProgress) 0f else 1f,
+        tween(500)
     )
     val motionHeight by animateDpAsState(
-        targetValue = if (lazyScrollState.firstVisibleItemIndex in 0..1) 288.dp else 64.dp,
-        tween(5000)
+//        targetValue = if (lazyScrollState.firstVisibleItemIndex in 0..1) 288.dp else 64.dp,
+//        targetValue = if (!lazyScrollState.canScrollBackward) 288.dp else 64.dp,
+        targetValue = if (!lazyScrollState.canScrollBackward && !lazyScrollState.isScrollInProgress) 288.dp else 64.dp,
+        tween(500)
     )
+    val corner = 16.dp - (16.dp * Math.abs(progress))
 
     MotionLayout(
         motionScene = MotionScene(content = motionScene),
         progress = progress,
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.Cyan)
             .height(motionHeight)
     ) {
         val boxProperties = customProperties(id = "box")
@@ -122,14 +94,17 @@ fun CollapsingToolbar(
         Box(
             modifier = Modifier
                 .layoutId("box")
-                .background(boxProperties.color("background"))
+                .clip(RoundedCornerShape(bottomStart = corner, bottomEnd = corner))
+                .background(Color(0xFFE0DFFD))
         )
         SmallFloatingActionButton(
             modifier = Modifier
                 .layoutId("back"),
-            onClick = { }
+            onClick = {
+                onBackClick()
+            }
         ) {
-            androidx.compose.material.Icon(
+            Icon(
                 painter = painterResource(id = dev.yacsa.ui.R.drawable.icon_caret_left_regular_24),
                 contentDescription = null,
             )
@@ -138,7 +113,8 @@ fun CollapsingToolbar(
         Card(
             modifier = Modifier.layoutId("img"),
             elevation = 4.dp,
-            shape = RoundedCornerShape(8.dp),
+            border = BorderStroke(1.dp, Color(0xFF46467A)),
+            shape = RoundedCornerShape(16.dp),
         ) {
             Image(
                 painter = painter,
@@ -150,8 +126,8 @@ fun CollapsingToolbar(
         }
 
         Text(
-            modifier = Modifier.layoutId("title"),
-            text = book.title?:"SWW",
+            modifier = Modifier.layoutId("title").basicMarquee(),
+            text = book.title ?: "SWW",
             style = YacsaTheme.typography.heading,
             maxLines = 2,
             textAlign = TextAlign.Start,
@@ -159,9 +135,10 @@ fun CollapsingToolbar(
 
         Text(
             modifier = Modifier.layoutId("author"),
-            text =book?.authors?.firstOrNull()?.name ?: "SWW",
+            text = book?.authors?.firstOrNull()?.name ?: "SWW",
             style = YacsaTheme.typography.title,
             maxLines = 2,
+            minLines = 2,
             textAlign = TextAlign.Start,
         )
 
@@ -169,12 +146,12 @@ fun CollapsingToolbar(
             modifier = Modifier.layoutId("download"),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            androidx.compose.material.Icon(
+            Icon(
                 painter = painterResource(id = dev.yacsa.ui.R.drawable.icon_archive_box_regular_16),
                 contentDescription = null,
             )
             Spacer(modifier = Modifier.width(4.dp))
-            androidx.compose.material.Text(
+            Text(
                 text = book.downloadCount?.toString() ?: "NI",
                 style = YacsaTheme.typography.caption,
             )
@@ -186,7 +163,15 @@ fun CollapsingToolbar(
             item {
                 SmallFloatingActionButton(onClick = { /*TODO*/ }) {
                     Icon(
-                        painter = painterResource(id = dev.yacsa.ui.R.drawable.icon_sun_regular_24),
+                        painter = painterResource(id = dev.yacsa.ui.R.drawable.icon_heart_regulat_24),
+                        contentDescription = null,
+                    )
+                }
+            }
+            item {
+                SmallFloatingActionButton(onClick = { onDownloadClick()}) {
+                    Icon(
+                        painter = painterResource(id = dev.yacsa.ui.R.drawable.icon_download_regular_24),
                         contentDescription = null,
                     )
                 }
@@ -194,15 +179,7 @@ fun CollapsingToolbar(
             item {
                 SmallFloatingActionButton(onClick = { /*TODO*/ }) {
                     Icon(
-                        painter = painterResource(id = dev.yacsa.ui.R.drawable.icon_moon_regular_24),
-                        contentDescription = null,
-                    )
-                }
-            }
-            item {
-                SmallFloatingActionButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        painter = painterResource(id = dev.yacsa.ui.R.drawable.icon_android_logo_regular_24),
+                        painter = painterResource(id = dev.yacsa.ui.R.drawable.icon_share_regular_24),
                         contentDescription = null,
                     )
                 }
@@ -215,8 +192,25 @@ fun CollapsingToolbar(
 
 @Preview(showBackground = true)
 @Composable
-fun Bar_Bar() {
+fun Preview_CollapsingToolbar() {
     YacsaTheme {
-        Bar(null)
+        CollapsingToolbar(
+            lazyScrollState = rememberLazyListState(),
+            book = BookUiModel(
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+            ),
+            onBackClick={},
+            onDownloadClick={}
+        )
     }
 }

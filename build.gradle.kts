f492bd298cc8d544cc809319dd822962ee30efe4
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-06-20 19:43:24 +0300

    YACSA-42 Add kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,4 +18,5 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     id("org.jmailen.kotlinter") version "3.14.0" apply false
+    id("org.jetbrains.kotlinx.kover") version "0.6.1"
 }

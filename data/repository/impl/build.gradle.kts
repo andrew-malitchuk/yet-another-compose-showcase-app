e8a7f0f23544ca9e7328e9a43f83445096819036
Andrew Malitchuk <andrew.malitchuk@gmail.com>
2023-07-09 10:07:56 +0300

    YACSA-61 Play around with unit testing

--- data/repository/impl/build.gradle.kts
+++ data/repository/impl/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
 
 android {
     namespace = "dev.yacsa.repository.impl"
+    defaultConfig {
+        testInstrumentationRunner = "dev.yacsa.repository.impl.HiltTestRunner"
+    }
 }
 
 dependencies {
@@ -24,4 +27,20 @@ dependencies {
      implementation(libs.arrow.optics)
     ksp(libs.arrow.optics.ksp.plugin)
 
+    //
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.test.ext:junit:1.1.5")
+    testImplementation("com.google.dagger:hilt-android-testing:2.44.2")
+    kaptTest("com.google.dagger:hilt-android-compiler:2.44.2")
+    testImplementation("androidx.test:core-ktx:1.5.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.7.20")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+    testImplementation("org.robolectric:robolectric:4.9")
+    implementation("androidx.test:runner:1.5.2")
+    testImplementation("io.mockk:mockk:1.13.5")
+    implementation(libs.faker)
+    testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
+    //
 }
